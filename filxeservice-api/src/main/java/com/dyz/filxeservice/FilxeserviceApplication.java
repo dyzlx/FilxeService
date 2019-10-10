e82e8cd2696d1d6d4aa8f9bc055f38c9669162cc
duyunze <13212068907@163.com>
2019-10-10 21:07:14 +0800

    Filxeservice init

---
+++ filxeservice-api/src/main/java/com/dyz/filxeservice/FilxeserviceApplication.java
@@ -0,0 +1,14 @@
+package com.dyz.filxeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilxeserviceApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FilxeserviceApplication.class, args);
+	}
+}
+

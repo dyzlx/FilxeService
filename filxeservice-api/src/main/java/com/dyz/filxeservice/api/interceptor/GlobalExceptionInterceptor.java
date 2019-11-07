441461e9aaad831107eceda940f5e658b5081bc9
duyunze <13212068907@163.com>
2019-11-07 22:00:58 +0800

    Support Feign Client

--- filxeservice-api/src/main/java/com/dyz/filxeservice/api/interceptor/GlobalExceptionInterceptor.java
+++ filxeservice-api/src/main/java/com/dyz/filxeservice/api/interceptor/GlobalExceptionInterceptor.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.dyz.filxeservice.api.model.Result;
 import com.dyz.filxeservice.common.execption.BusinessException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionInterceptor {
 
 	@ExceptionHandler(value = BusinessException.class)
 	public ResponseEntity<Result> handlerBusinessException(BusinessException exception) {
+		log.error("filxeservice handle fail", exception);
 		return ResponseEntity.status(HttpStatus.FORBIDDEN)
 				.body(Result.builder().code(exception.getCode()).message(exception.getMessage()).build());
 	}
 	
 	@ExceptionHandler(value = Exception.class)
 	public ResponseEntity<Result> handlerException(Exception exception) {
+		log.error("filxeservice handle fail", exception);
 		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
 				.body(Result.builder().code(-1).message(exception.getMessage()).build());
 	}
--- filxeservice-client/src/main/java/com/dyz/filxeservice/client/LogicFileClient.java
+++ filxeservice-client/src/main/java/com/dyz/filxeservice/client/LogicFileClient.java
@@ -2,13 +2,39 @@ package com.dyz.filxeservice.client;
 
 import java.util.List;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+
+@FeignClient("filxeservice")
 public interface LogicFileClient {
 
-	void deleteLogicFile(Integer logicFileId, Integer userId);
+	@RequestMapping(value = "/filxeservice/logicfiles/{logicFileId}", method = RequestMethod.DELETE,
+			consumes = { "application/json","application/xml" })
+	void deleteLogicFile(@PathVariable(name = "logicFileId") Integer logicFileId,
+			@RequestHeader(name = "userId") Integer userId);
 	
-	void deleteLogicFiles(List<Integer> logicFileIds, Integer userId);
+	@RequestMapping(value = "/filxeservice/logicfiles", method = RequestMethod.DELETE,
+			consumes = { "application/json","application/xml" })
+	void deleteLogicFiles(@RequestBody List<Integer> logicFileIds,
+			@RequestHeader(name = "userId") Integer userId);
 	
-	List<Integer> uploadFiles(MultipartFile[] files, boolean ishared, Integer userId);
+	@RequestMapping(value = "/filxeservice/logicfiles/upload", method = RequestMethod.POST,
+			consumes = { "multipart/form-data" })
+	List<Integer> uploadFiles(@RequestPart(name = "file") MultipartFile[] file,
+			@RequestHeader(name = "userId") Integer userId);
+	
+//	@Configuration
+//    class MultipartSupportConfig {
+//        @Bean
+//        public Encoder feignFormEncoder() {
+//            return new SpringFormEncoder();
+//        }
+//    }
 }
